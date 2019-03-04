e83ef05f45826bfdd8ea2b08ef5a53e2c4b56fa6
mostafAanwar <uhzybh8h>
2019-03-04 19:51:48 +0200

    Added main classes will be working on

---
+++ src/Block.java
@@ -0,0 +1,21 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Block {
+    Block() {
+        String timeStamp = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date());
+    }
+
+    public static void main(String[] args) {
+        int index;
+        String timeStamp;
+        //System.out.println(timeStamp);
+        String[] data;
+        String hash;
+        String prevHash;
+    }
+
+    String hash_block() {
+        return "";
+    }
+}
