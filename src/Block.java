877a264cffb752f64867fe2d1d47379d315b1674
mostafAanwar <uhzybh8h>
2019-03-04 19:53:58 +0200

    edited code of block class

--- src/Block.java
+++ src/Block.java
@@ -3,13 +3,13 @@ import java.util.Date;
 
 public class Block {
     Block() {
-        String timeStamp = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date());
+
     }
 
     public static void main(String[] args) {
         int index;
-        String timeStamp;
-        //System.out.println(timeStamp);
+        String timeStamp = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date());
+        System.out.println(timeStamp);
         String[] data;
         String hash;
         String prevHash;
