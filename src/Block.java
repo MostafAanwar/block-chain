fe95a331f6a003fae39fac8f6608a71cb9f52c92
mostafAanwar <uhzybh8h>
2019-03-05 19:32:40 +0200

    edited the constructor of the blocks

--- src/Block.java
+++ src/Block.java
@@ -3,14 +3,15 @@ import java.util.Date;
 import java.util.*;
 
 public class Block {
-    private static String timeStamp;
-    int index;
-    String[] data;
-    String hash;
-    String prevHash;
+    private static long timeStamp;
+    public String hash;
+    public String previousHash;
+    private String data;
 
-    Block() {
-        timeStamp = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date());
+    public Block(String data, String previousHash) {
+        this.data = data;
+        this.previousHash = previousHash;
+        timeStamp = new Date().getTime();
     }
 
     public static void main(String[] args) {
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         //System.out.println("Hello World!");
-        Block b = new Block();
+        //Block b = new Block();
     }
 }
